56a4d3763bca921becdddaab1126a172efdc3b37
Mitali Gohel <mitaligohel.bs@gmail.com>
2026-03-23 16:00:51 +0530

    emergency fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,18 @@
 import Landing from "@/components/Landing/Landing";
 
 export const metadata = {
+  metadataBase: new URL("https://skillpathdev.vercel.app"),
   title: "Practice Coding MCQs Online – Improve Your Skills | SkillDev",
   description:
-    "Start practicing coding MCQs online with SkillDev. Explore features, test your knowledge, and level up your programming skills quickly.",
-  alternates: {
-    canonical: `https://skillpathdev.vercel.app/`,
-  },
+    "Start practicing coding MCQs online with SkillDev. Explore features and level up your programming skills.",
   openGraph: {
     title: "SkillPathDev – Practice Coding MCQs & Crack Tech Interviews",
-    description:
-      "Master JavaScript, React & more with topic-wise MCQs. Track progress, analyze performance, and prepare for real developer interviews on SkillPathDev.",
-    url: "https://skillpathdev.vercel.app/",
+    description: "Master JavaScript, React & more with topic-wise MCQs.",
+    url: "/",
     siteName: "SkillPathDev",
     images: [
       {
-        url: "https://skillpathdev.vercel.app/landing.png",
+        url: "/landing.png",
         width: 1200,
         height: 630,
         alt: "SkillPathDev Coding Practice Platform",
